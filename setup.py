811f804a86ab525071f36007721acefae39be37e
moritzt <moritz.thuerlemann@phys.chem.ethz.ch>
2023-04-17 15:48:41 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
     description='Energy Based Clustering',
     author='Moritz Thürlemann',
     author_email='moritz.thuerlemann@phys.chem.ethz.ch',
-    packages=find_packages(include=['EBC']),
-    package_dir={'':'source'},
+    package_dir={'':'ebc'},
     install_requires=["scipy>=1.9.0", "scikit-learn>=1.0.1", "networkx>=2.8.8", "matplotlib>=3.5.3", "numpy>=1.19.5"]
 )
--- source/__init__.py
+++
@@ -1,1 +0,0 @@
-from EBC import EBC
